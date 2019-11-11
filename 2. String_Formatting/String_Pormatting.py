d6c04feba1b9a80b6987b0b81ae9e2a0a01eb99e
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2019-11-11 18:21:04 +0700

    String Formatting

---
+++ 2. String_Formatting/String_Pormatting.py
@@ -0,0 +1,17 @@
+nama = "Arif"
+tahun = 20
+list = [1, 2, 3]
+
+print("Hello, %s!" % nama)
+
+print("%s berusia %d" % (nama, tahun))
+
+print("Data List : %s" % list)
+
+'''
+%s - String (or any object with a string representation, like numbers)
+%d - Integers
+%f - Floating point numbers
+%.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+%x/%X - Integers in hex representation (lowercase/uppercase)
+'''
