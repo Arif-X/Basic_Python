e2a93e03e3682947d954ef0e14166c4205ccf3a2
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2019-11-13 00:04:01 +0700

    Basic Operation

---
+++ 3. Basic_Operation/Basic_Operation.py
@@ -0,0 +1,38 @@
+# Operator Aritmatik
+# Printout 5.0
+angka = 1 - 2 + 3 * 4 / 2
+print(angka)
+
+# Modulus
+# Printout 1
+modulus = 5 % 2
+print(modulus)
+
+# Perpangkatan
+# Printout 49
+pangkat = 7 ** 2
+print(pangkat)
+
+# Pertambahan String
+# Printout hello world
+strHelloWorld = "hello" + " " + "world"
+print(strHelloWorld)
+
+# Pengulangan String dengan Perkalian
+# Printout hello hello hello hello hello hello hello hello hello hello
+strHello = "hello " * 10
+print(strHello)
+
+# Print Semua List
+# Printout [1, 2, 3, 4, 5, 6, 7, 8]
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+semuaList = list1 + list2
+print(semuaList)
+
+# Print List dengan Perkalian
+# Printout [1, 2, 3, 1, 2, 3, 1, 2, 3]
+list = [1, 2, 3]
+pengkali = 3
+operasiList = list * pengkali
+print(operasiList)
